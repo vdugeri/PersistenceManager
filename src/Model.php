97bfa69dc33153723003d6197ff60db7bd26060b
andela-vdugeri <verem.dugeri@andela.co>
2015-09-26 09:40:17 +0100

    Add class for pluralizing model names

--- src/Model.php
+++ src/Model.php
@@ -10,31 +10,88 @@ namespace Verem\persistence;
 
 abstract class Model
 {
+	private $class;
+
     public function __construct()
     {
+		$reflection = new \ReflectionClass($this);
+		$this->class = $reflection->getName();
     }
 
+	/**
+	 * @return string
+	 *
+	 * get the name of the class. This will determine
+	 * the name of the table
+	 */
+	protected function getClass()
+	{
+		return $this->class;
+	}
 
-    public function all()
+
+	/**
+	 * return all records in the database
+	 */
+    public static function all()
     {
+
     }
 
-    public function create($table, $values = array())
+	/**
+	 * @param array $values
+	 *
+	 * create a record in the given table
+	 */
+    public function create($values = array())
     {
+		//TODO: change this to get the name of the table
+		//TODO: with camel cases splited to underscores
+		//TODO: and the name of the class pluralised.
+
+		$table = $this->getTable($this->getClass());
     }
+
+	/**
+	 * @param $id
+	 *
+	 * find and return a record from db
+	 */
+
+	protected function getTable($class)
+	{
+		$table = strtolower($this->class);
+		return $table;
+	}
     public function find($id)
     {
     }
 
-
+	/**
+	 * @param $column
+	 * @param $value
+	 *
+	 * find all records where column matches value
+	 */
     public function where($column, $value)
     {
     }
 
-    public function update($id, $value = array())
+	/**
+	 * @param $id
+	 * @param array $values
+	 *
+	 * Update a row in the db with a matching id
+	 */
+    public function update($id, $values = array())
     {
     }
 
+	/**
+	 * @param $id
+	 *
+	 * Delete a record from the database
+	 */
     public function destroy($id)
     {
     }
---
+++ tests/InflectTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andela
+ * Date: 9/26/15
+ * Time: 9:30 AM
+ */
+
+namespace Verem\persistence\Test;
+
+
+use Verem\persistence\Inflect;
+
+class InflectTest extends \PHPUnit_Framework_TestCase {
+
+
+	/**
+	 * @param $expected
+	 * @param $actual
+	 *
+	 * @dataProvider wordProvider
+	 */
+	public function testWordInflection($actual, $expected )
+	{
+		$this->assertEquals($expected, Inflect::pluralize($actual));
+	}
+
+	public function wordProvider()
+	{
+		return [
+			['move', 'moves'],
+			['movie', 'movies'],
+			['hand', 'hands'],
+			['child', 'children'],
+			['user', 'users'],
+			['foot', 'feet']
+		];
+	}
+}
