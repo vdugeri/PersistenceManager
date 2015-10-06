1dbd52914c268dfaec7c32218f614d65de095348
andela-vdugeri <verem.dugeri@andela.co>
2015-10-06 11:21:10 +0100

    Remove ugly comments

--- src/Exceptions/DatabaseException.php
+++ src/Exceptions/DatabaseException.php
@@ -19,7 +19,7 @@ class DatabaseException extends PDOException {
 	{
 		if(strstr($e->getMessage(), 'SQLSTATE[')) {
 			preg_match('/SQLSTATE\[(\w+)\] \[(\w+)\] (.*)/', $e->getMessage(), $matches);
-			
+
 			$this->code = ($matches[1] == 'HT000' ? $matches[2] : $matches[1]);
 			$this->message = $matches[3];
 		}
